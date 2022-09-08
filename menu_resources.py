c8827c40935c1b0c861d64ac44741ce0eb43fd74
BayleyB <bay8142@gmail.com>
2022-09-08 11:19:24 -0700

    Updtaed about page in main menu

--- menu_resources.py
+++ menu_resources.py
@@ -1,12 +1,13 @@
 import PySimpleGUI as psg
 
 def run_about_window(large_font, small_font, bg_color, menu_button_color, window_width, window_height):
-    about_layout = [[psg.Text("This game is really fun", justification='center', font=small_font, background_color=bg_color)],
+    about_layout = [[psg.Text("This game was developed over the course of Summer 2022\nby Bayley Barreuther\n\nSee GitHub for details:\nhttps://github.com/BayleyB/SummerProject2022", justification='center', font=small_font, background_color=bg_color)],
                     [psg.Button("Close", button_color=menu_button_color)]]
 
     about_window = psg.Window("About", about_layout,
                               background_color=bg_color,
-                              size=(window_width, window_height))
+                              size=(window_width, window_height),
+                              element_justification='c')
 
     while True:
         about_event, _ = about_window.read()
